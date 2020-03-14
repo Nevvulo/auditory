feac76b26544af63b6c503910acb888625500531
Blake <bsward8@gmail.com>
2020-03-14 17:09:00 +1100

    feat: new functions for menu

---
+++ src/menu.js
@@ -0,0 +1,21 @@
+function openNav() {
+  const container = document.querySelector('.container');
+  const visualizerContainer = document.querySelector('.visualizer-container');
+  const menu = document.querySelector('.menu');
+  container.style.backgroundColor = 'rgba(0, 0, 0, 0.7)'
+  menu.style.width = '250px';
+  menu.style.padding = '16px';
+  menu.style.paddingTop = '52px';
+  visualizerContainer.style.marginLeft = '250px';
+};
+
+function closeNav() {
+  const container = document.querySelector('.container');
+  const visualizerContainer = document.querySelector('.visualizer-container');
+  const menu = document.querySelector('.menu');
+  container.style.backgroundColor = 'transparent'
+  menu.style.width = '0px';
+  menu.style.padding = '0px';
+  menu.style.paddingTop = '52px';
+  visualizerContainer.style.marginLeft = '0px';
+};
